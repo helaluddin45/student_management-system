3cde2c3c9e9be31a737b85a880f021e1b710decf
helaluddin45 <helaluddi802@gmail.com>
2026-09-18 18:09:28 +0600

    Added Student class with display_info method

--- hw.py
+++ hw.py
@@ -1,4 +1,10 @@
-def greet(name):
-    return f"Hello, {name}!"
+class Student:
+    def __init__(self, name, roll):
+        self.name = name
+        self.roll = roll
 
-print(greet("Saida"))
+    def display_info(self):
+        return f"Name: {self.name}, Roll: {self.roll}"
+
+student1 = Student("Saida", 101)
+print(student1.display_info())
